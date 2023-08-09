13c6c757568be3350274c0cd6e059d66cef1caab
Maxime Thibaut <max.thibaut164@gmail.com>
2023-08-09 21:06:37 +0200

    [ADD] show/hidden pool buttons on click

--- static/js/menu.js
+++ static/js/menu.js
@@ -1,24 +1,44 @@
 function displayRecipeSubMenu() {
-    for (let x = 1; x < 4; x++ ) {
-        let button = document.createElement('input');
-        button.setAttribute("class", "recipe_sub_button");
-        button.setAttribute("id", "recipe_sub_button");
-        button.setAttribute("type", "button");
-        button.setAttribute("value", "Pool " + x);
-        button.setAttribute("onclick", "moveToGameWindow("+x+", 'recipe')");
-        document.getElementById('recipe_sub_buttons').appendChild(button);
+    let subButton = document.getElementById('recipe_sub_buttons');
+
+    if (document.getElementById('recipe_sub_button')) {
+        
+        while (subButton.firstChild) {
+            subButton.removeChild(subButton.firstChild);
+        }
+    }
+    else {        
+        for (let x = 1; x < 4; x++ ) {
+            let button = document.createElement('input');
+            button.setAttribute("class", "recipe_sub_button");
+            button.setAttribute("id", "recipe_sub_button");
+            button.setAttribute("type", "button");
+            button.setAttribute("value", "Pool " + x);
+            button.setAttribute("onclick", "moveToGameWindow("+x+", 'recipe')");
+            document.getElementById('recipe_sub_buttons').appendChild(button);
+        }
     }
 }
 
 function displayMovieSubMenu() {
-    for (let x = 1; x < 4; x++ ) {
-        let button = document.createElement('input');
-        button.setAttribute("class", "movie_sub_button");
-        button.setAttribute("id", "movie_sub_button");
-        button.setAttribute("type", "button");
-        button.setAttribute("value", "Pool " + x);
-        button.setAttribute("onclick", "moveToGameWindow("+x+", 'movie')");
-        document.getElementById('movie_sub_buttons').appendChild(button);
+    let subButton = document.getElementById('movie_sub_buttons');
+
+    if (document.getElementById('movie_sub_button')) {
+        
+        while (subButton.firstChild) {
+            subButton.removeChild(subButton.firstChild);
+        }
+    }
+    else {  
+        for (let x = 1; x < 4; x++ ) {
+            let button = document.createElement('input');
+            button.setAttribute("class", "movie_sub_button");
+            button.setAttribute("id", "movie_sub_button");
+            button.setAttribute("type", "button");
+            button.setAttribute("value", "Pool " + x);
+            button.setAttribute("onclick", "moveToGameWindow("+x+", 'movie')");
+            document.getElementById('movie_sub_buttons').appendChild(button);
+        }
     }
 }
 
